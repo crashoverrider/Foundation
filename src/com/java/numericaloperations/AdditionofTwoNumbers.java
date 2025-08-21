6aa71629d87cfb282e3a68f629df4d03bd06bfae
Satish Rajana <satishrajana@live.in>
2025-08-21 12:27:09 +0530

    Committing addition of two numbers.

---
+++ src/com/java/numericaloperations/AdditionofTwoNumbers.java
@@ -0,0 +1,27 @@
+package com.java.numericaloperations;
+
+import java.util.Scanner;
+
+public class AdditionofTwoNumbers {
+
+	public static void main(String[] args) {
+		
+//		System.out.println("Demo");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		int c = a+b;
+		
+		System.out.println(c);
+		
+		scan.close();
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
